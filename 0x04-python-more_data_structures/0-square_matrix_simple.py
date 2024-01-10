84ca9b00218b266bfd2e390f58e5fd581794a32d
SeipatiP <sayparty1@gmail.com>
2024-01-10 12:22:39 -0800

    Function that computes value of integers

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-
-    for i in range(len(matrix)):
-        new_matrix[1] = list(map(lambda x: x**2, matrix[1]))
-
-    return (new_matrix)
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
